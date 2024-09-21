4e35e4cfc56f4065c84de6600b881d8ebece495c
Jan Sebastian Siwy <github@jansiwy.de>
2024-09-21 19:58:02 +0200

    Add support for resource-specific tags

--- variables.tf
+++ variables.tf
@@ -1,15 +1,33 @@
 variable "client_id_list" {
-  type        = list(string)
-  description = "List of client IDs, also known as audiences"
+  type = list(string)
+
+  description = <<EOS
+List of client IDs, also known as audiences.
+EOS
 }
 
-variable "tags" {
-  type        = map(string)
-  default     = {}
-  description = "Tags to be attached to the IAM OpenID Connect provider"
+variable "default_tags" {
+  type    = map(string)
+  default = {}
+
+  description = <<EOS
+Map of tags assigned to all AWS resources created by this module.
+EOS
+}
+
+variable "iam_openid_connect_provider_tags" {
+  type    = map(string)
+  default = {}
+
+  description = <<EOS
+Map of tags assigned to the IAM OpenID Connect Provider.
+EOS
 }
 
 variable "url" {
-  type        = string
-  description = "URL of the OpenID provider"
+  type = string
+
+  description = <<EOS
+URL of the OpenID provider.
+EOS
 }
